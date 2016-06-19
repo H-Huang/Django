00b8a5ffc1301dbe6063f5c198947e5b92a17089
Howard Huang <howardhuang96@gmail.com>
2016-06-19 00:22:46 -0700

    8

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
-from blogs.models import Post
+from blog.models import Post
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Post.object.all().order_by("-date")[:25]),
-        template_name="blog/blog.html")
+                url(r'^$', ListView.as_view(
+                                    queryset=Post.objects.all().order_by("-date")[:25],
+                                    template_name="blog/blog.html")),
 ]
